c224e7d146cb69df04234fb15defa86a9cd7ed59
RohitSingh <thakurrohitdsingh@gmail.com>
2023-07-09 23:49:40 +0530

    Added <spring-cloud-starter-netflix-eureka-client> dependency Added <spring-cloud.version>2021.0.8</spring-cloud.version> in perperties tag
    Added <dependencyManagement> Dependency
    
    Also Added @EnableDiscoveryClient

--- src/main/java/com/singh/base/Supplier_MS.java
+++ src/main/java/com/singh/base/Supplier_MS.java
@@ -3,10 +3,12 @@ package com.singh.base;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication // Supplier Microservice
+@EnableDiscoveryClient
 public class Supplier_MS {
 
 	public static void main(String[] args) {
